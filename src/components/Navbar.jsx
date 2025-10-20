96669d0543ce37cf99e2cb2eb7f6006e532193d7
Your Name <michaeliskingbd@gmail.com>
2025-10-20 12:09:53 +0100

    navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>LOGO</h1>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
