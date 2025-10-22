b7f7517b966308994c4ce3965022c1ae9b268d6c
michaeliskingbd <michaeliskingbd@gmail.com>
2025-10-22 13:45:14 +0100

    nAVBAR

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <nav>
+        <h1>Logo</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Listing</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Homepage;
