73dfd6f7514bb84ca75818ab1860cdcf6c47f8f5
Toheeb-git <addmore12000@gmail.com>
2025-10-25 08:39:26 +0100

    Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">Hello world</h1>
+      <Home />
     </div>
   );
 }
