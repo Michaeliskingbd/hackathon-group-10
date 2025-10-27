21e5de5dce668e83b711b2c182c1394f502e9ddd
Toheeb-git <addmore12000@gmail.com>
2025-10-27 05:44:37 +0000

    Updated product page

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/Logo.svg"
 import logo2 from "../assets/logo2.png"
 import logo3 from "../assets/logo3.png"
 import logo4 from "../assets/logo4.png"
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoClose } from 'react-icons/io5'
+import { SiCookiecutter } from 'react-icons/si'
+import { FaFacebook } from 'react-icons/fa'
+import { BsInstagram, BsTwitter } from 'react-icons/bs'
+import { LiaLinkedin } from 'react-icons/lia'
 
 const ProductPage = () => {
+
+    const [isMobileNav, setIsMobileNav ] = useState("false");
+
+    const mobileNav = () => {
+        setIsMobileNav(!isMobileNav);
+    }
   return (
-    <section className=''>
-        <nav className='flex justify-between px-20 py-5'>
+    <section className='bg-[#def1ef]'>
+        <nav className='flex justify-between lg:px-20 py-5'>
             <img src={logo}></img>
             <ul className='lg:flex gap-7 capitalize text-[#6EC1E4] text-lg font-["plus jakarta Sans, san serif"] cursor-pointer hidden'>
                 <li className='hover:text-orange-400'>home</li>
@@ -16,6 +28,7 @@ const ProductPage = () => {
                 <li className='hover:text-orange-400'>Features</li>
                 <li className='hover:text-orange-400'>contact</li>
             </ul>
+            <button onClick={mobileNav} className='lg:hidden pr-2 bg-white text-[#31587c] px-3 py-2 text-xl border-[1px] border-[#6EC1E4]'>{isMobileNav? <IoClose/> :<GiHamburgerMenu />}</button>
         </nav>
         <div className=' h-40 flex flex-col gap-7 mt-[82px] justify-center w-full'>
           <h1 className='text-[68px] text-[#31587c] font-[600] text-center capitalize font-["plus jakarta Sans, san serif"]'>products</h1>
@@ -159,6 +172,71 @@ const ProductPage = () => {
              <button className='bg-[#f2a341] uppercase px-7 py-4 rounded-full text-[#fff] text-base font-bold font-["poppins, san serif"] hover:bg-white hover:text-[#f2a341]'>get started today!</button>
             </div>
           </section>
+
+          {/* Footer */}
+      <footer className="bg-gray-200  text-cyan-700 font-bold mt-20 py-12 px-8">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          
+          {/* Menu */}
+          <div>
+            <h3 className="text-lg font-bold mb-4 text-blue-950">Menu</h3>
+            <ul className="space-y-2 text-lg font-semibold">
+              <li><a href="/" className="hover:text-orange-400">Home</a></li>
+              <li><a href="/about" className="hover:text-orange-400">About Us</a></li>
+              <li><a href="/services" className="hover:text-orange-400">Products</a></li>
+              <li><a href="/services" className="hover:text-orange-400">Features</a></li>
+              <li><a href="/contact" className="hover:text-orange-400">Contact</a></li>
+            </ul>
+          </div>
+
+          {/* Quick Links */}
+          <div>
+            <h3 className="text-lg font-bold mb-4 text-blue-950">Quick Links</h3>
+            <ul className="space-y-2 text-lg font-semibold">
+              <li><a href="/" className="hover:text-orange-400">Blog</a></li>
+              <li><a href="/about" className="hover:text-orange-400">Support</a></li>
+              <li><a href="/services" className="hover:text-orange-400">Careers</a></li>
+              <li><a href="/contact" className="hover:text-orange-400">Privacy Policy</a></li>
+            </ul>
+          </div>
+
+          {/* Contacts */}
+          <div>
+            <h3 className="text-lg font-bold mb-4 text-blue-950">Contact</h3>
+            <ul className="space-y-2 text-lg font-semibold">
+              <p>Appclick Tech Academy 10, Soun Ajagungbade Street <br /> Bodija Ibadan.</p>
+            </ul>
+          </div>
+
+          {/* Media icons */}
+          <div>
+            <h3 className="text-lg font-bold mb-4 text-blue-950">Follow Us</h3>
+            <div className="flex space-x-4">
+              <a href="#" className="hover:text-orange-400"><FaFacebook size={20} /></a>
+              <a href="#" className="hover:text-orange-400"><BsTwitter size={20} /></a>
+              <a href="#" className="hover:text-orange-400"><BsInstagram size={20} /></a>
+              <a href="#" className="hover:text-orange-400"><LiaLinkedin size={20} /></a>
+            </div>
+                </div>
+        </div>
+
+        {/* Copyright Part */}
+        <div className="text-center text-gray-400 text-sm mt-10 border-t border-gray-700 pt-6">
+          © {new Date().getFullYear()} Hackhathon Group 10 — All Rights Reserved.
+        </div>
+        </footer>
+
+          {/* Mobile navbar */}
+
+         {isMobileNav && <nav className='bg-[#ccf5ee] absolute top-0 w-[100%] mt-28 h-[400px] lg:hidden'>
+           <ul className='capitalize flex flex-col gap-10 px-4 pt-12 text-base'>
+            <li className=' text-[#1ca6af] cursor-pointer hover:text-[#f2a341]'>home</li>
+            <li className=' text-[#1ca6af] cursor-pointer hover:text-[#f2a341]'>about us</li>
+            <li className=' text-[#1ca6af] cursor-pointer hover:text-[#f2a341]'>product</li>
+            <li className=' text-[#1ca6af] cursor-pointer hover:text-[#f2a341]'>features</li>
+            <li className=' text-[#1ca6af] cursor-pointer hover:text-[#f2a341]'>contact</li>
+            </ul> 
+          </nav>}
     </section>
   )
 }
