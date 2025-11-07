ca703519eb71030408c5a1783679bcc262997b95
OlatundeAdebesin <lovemineafl@gmail.com>
2025-11-07 10:55:50 +0100

    Full Home Page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,20 @@
-import Logo from '../assets/Logo.svg';
+import { IoMenu, IoClose } from "react-icons/io5";
 
-const NavBar = () => {
-    return (
-<section>
-<nav className='flex justify-between px-20 py-5'>
-            <img src={Logo}></img>
+
+import React, { useState } from "react";
+
+
+
+
+const Navbar = () => {
+    const [isMobileNav, setIsMobileNav ] = useState("false");
+
+    const mobileNav = () => {
+        setIsMobileNav(!isMobileNav);
+    }
+  return (
+    <nav className='flex justify-between lg:px-20 py-5'>
+            {/* <img src={logo}></img> */}
             <ul className='lg:flex gap-7 capitalize text-[#6EC1E4] text-lg font-["plus jakarta Sans, san serif"] cursor-pointer hidden'>
                 <li className='hover:text-orange-400'>home</li>
                 <li className='hover:text-orange-400'>about us</li>
@@ -12,8 +22,9 @@ const NavBar = () => {
                 <li className='hover:text-orange-400'>Features</li>
                 <li className='hover:text-orange-400'>contact</li>
             </ul>
+            <button onClick={mobileNav} className='lg:hidden pr-2 bg-white text-[#31587c] px-3 py-2 text-xl border-[1px] border-[#6EC1E4]'>{isMobileNav? <IoClose /> :<GiHamburgerMenu />}</button>
         </nav>
-</section>
-    );
-}
-export default NavBar;
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
+import ProductPage from '../Components/ProductPage'
 
-import NavBar from '../components/NavBar'
-import HeroSection from '../components/HeroSection'
-import Activities from '../components/Activities'
-import Products from '../components/Products'
-import Partners from '../components/Partners'
-import Testimonials from '../components/Testimonials'
-import Request from '../components/Request'
-import Footer from '../components/Footer'
 const Home = () => {
   return (
     <section>
@@ -23,7 +16,6 @@ const Home = () => {
     </div>
 
     </section>
-
   )
 }
 
